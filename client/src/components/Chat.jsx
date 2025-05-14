1bc22aeb7103cdfb65a16f468ec096b53abd8867
Fik0u <fikouch@gmail.com>
2025-05-14 10:34:48 +0100

    Messages

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 import socket from '../socket';
 
 const Chat = ({ currentUser }) => {
@@ -23,18 +24,43 @@ const Chat = ({ currentUser }) => {
         });
         return () => {
             socket.off('getUsers');
-            socket.off('getMessage')
+            socket.off('getMessage');
         };
     }, [currentUser]);
 
-    const sendMessage = () => {
+    useEffect(() => {
+        const foundMessages = async () => {
+            if (!receiverId || receiverId === currentUser._id) return;
+
+            if (receiverId) {
+                try {
+                    const result = await axios.get(`/api/messages/${currentUser._id}/${receiverId}`);
+                    setMessages(result.data);
+                } catch (error) {
+                    console.error("Couldn't fetch messages", error)
+                }
+            }
+        };
+        foundMessages()
+    }, [receiverId, currentUser._id]);
+
+    const sendMessage = async () => {
         if (message && receiverId) {
             socket.emit('sendMessage', {
                 senderId: currentUser._id,
                 receiverId, 
                 text: message
             });
-            setMessages((prevMessages) => [...prevMessages, { senderId: currentUser._id, text: message }]);
+
+            try {
+                await axios.post('/api/messages', {
+                    senderId: currentUser._id,
+                    receiverId,
+                    text: message
+                })
+            } catch (error) {
+                console.error("Couldn't send message", error)
+            }
             setMessage('')
         }
     };
@@ -43,13 +69,16 @@ const Chat = ({ currentUser }) => {
     <div>
       <div>
         <h3>Online Users</h3>
-        {onlineUsers.map((user) => (
+        {onlineUsers
+        .filter(user => user.userId !== currentUser._id)
+        .map((user) => (
             <div key={`${user.userId}-${user.socketId}`} onClick={() => setReceiverId(user.userId)}>
                 {user.username} 
                 {user.userId === receiverId && '✅'}
             </div>
         ))}
       </div>
+
       <div>
         <h3>Messages</h3>
         {messages.map((msg, index) => (
