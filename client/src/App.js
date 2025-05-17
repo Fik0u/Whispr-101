bb997c922d1890351b3db121339fe044809bd0e1
Fik0u <fikouch@gmail.com>
2025-05-17 10:16:42 +0100

    Secure Routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,18 @@ import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Error from './pages/Error';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { currentUser } from './JS/actions/authAction';
 import Profile from './pages/Profile';
 import NavBar from './components/NavBar';
+import Chat from './components/Chat';
+import GroupChat from './components/GroupChat';
 
 function App() {
 
   const dispatch = useDispatch();
+  const isAuth = useSelector(state => state.authReducer.isAuth)
 
   useEffect(() => {
     if(localStorage.getItem('token')) {
@@ -28,7 +31,15 @@ function App() {
         <Route path = '/' element = {<Home />} />
         <Route path = '/register' element = {<Register />} />
         <Route path = '/login' element = {<Login />} />
+        
+        {isAuth && (
+          <>
         <Route path = '/profile' element = {<Profile />} />
+        <Route path = '/chats' element = {<Chat />} />
+        <Route path = '/groups' element = {<GroupChat />} />
+          </>
+        )}
+
         <Route path = '/*' element = {<Error />} />
       </Routes>
     </div>
